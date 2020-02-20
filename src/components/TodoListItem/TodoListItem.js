f21339aa385d9ac7a30a3e8060fc6aa59a8abe21
Olga Ioffe <yoffic@yahoo.com>
2020-02-20 23:26:53 +0300

    add ability to check done item and mark important

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -3,33 +3,54 @@ import React, { Component } from 'react';
 import './TodoListItem.css';
 
 export default class TodoListItem extends Component {
+  state = {
+    done: false,
+    important: false,
+  };
+
   onLabelClick = () => {
-    console.log(this.props.label);
-  }
+    this.setState((state) => {
+      return {
+        done: !state.done,
+      };
+    });
+  };
+
+  onMarkImportant = () => {
+    this.setState((state) => {
+      return {
+        important: !state.important,
+      };
+    });
+  };
 
   render() {
-    const { label, important = false } = this.props;
-
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal',
-    };
+    const { label, onDeleted } = this.props;
+    const { done, important } = this.state;
+    const itemClasses = ['todo-list-item'];
+    if (done) {
+      itemClasses.push('done');
+    }
+    if (important) {
+      itemClasses.push('important');
+    }
 
     return (
-      <span className="todo-list-item">
+      <span className={itemClasses.join(' ')}>
         <span 
           className="todo-list-item-label"
-          onClick={ this.onLabelClick } 
-          style={style}>
+          onClick={ this.onLabelClick }>
           { label }
         </span>
         <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={ this.onMarkImportant }>
           <i className="fa fa-exclamation" />
         </button>
 
         <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
+                className="btn btn-outline-danger btn-sm float-right"
+                onClick={ onDeleted }>
           <i className="fa fa-trash-o" />
         </button>
       </span>
