5ff8bfff6604781f7eb91129477a13720b320751
Olga Ioffe <yoffic@yahoo.com>
2020-02-22 21:51:18 +0300

    add input to create custom todo item

--- src/components/AddTaskPanel/AddTaskPanel.js
+++ src/components/AddTaskPanel/AddTaskPanel.js
@@ -3,13 +3,37 @@ import React, { Component } from 'react';
 import './AddTaskPanel.css';
 
 export default class AddTaskPanel extends Component {
-  render() {
-    const { onAdded } = this.props;
+  state = {
+    label: ''
+  };
 
+  onInputChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAdded(this.state.label);
+    this.setState({
+      label: ''
+    });
+  };
+
+  render() {
     return (
-      <div className="mt-3 add-task-panel">
-        <button className="btn btn-outline-info" onClick={ () => onAdded('Hello') }>Add Task</button>
-      </div>
+      <form 
+        className="mt-3 add-task-panel d-flex" 
+        onSubmit={ this.onSubmit }>
+        <input 
+          type="text"
+          className="form-control mr-2 add-task-input"
+          onChange={ this.onInputChange }
+          placeholder="Type task here"
+          value={ this.state.label } />
+        <button className="btn btn-outline-info">Add Task</button>
+      </form>
     );
   }
 };
