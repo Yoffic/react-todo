f92a5c3cec598f4e59c1885e6dae85021ff96b58
Olga Ioffe <yoffic@yahoo.com>
2020-02-22 22:00:11 +0300

    Updates

---
+++ precache-manifest.3a32275304e35f19fa1024502b92f957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "366843cdbefd2329c502dbf8db2cdb43",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "f5df5c38d5c115f874c0",
+    "url": "/react-todo/static/css/main.a32aef48.chunk.css"
+  },
+  {
+    "revision": "23e6e7645b196a6f3c6a",
+    "url": "/react-todo/static/js/2.9f835767.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-todo/static/js/2.9f835767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5df5c38d5c115f874c0",
+    "url": "/react-todo/static/js/main.e7c385aa.chunk.js"
+  },
+  {
+    "revision": "3607658cb9c0653a6a9a",
+    "url": "/react-todo/static/js/runtime-main.63ea5226.js"
+  }
+]);
